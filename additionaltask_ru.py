9026fa3b2cfa0ca25c816e17339255d0793e023c
tung1712003 <145548957+tung1712003@users.noreply.github.com>
2023-09-30 23:52:11 +0300

    Update additionaltask_ru.py

--- additionaltask_ru.py
+++ additionaltask_ru.py
@@ -1,22 +1,11 @@
-# import csv
+import csv
 
-# with open('books.csv', 'r') as file:
-#     a = csv.reader(file, delimiter=';')
-#     b = (sorted(a, reverse=True,  key=lambda a: a[8]))[1:21]
+with open('books.csv', 'r') as file:
+    a = csv.reader(file, delimiter=';')
+    b = (sorted(a, reverse=True,  key=lambda a: a[8]))[1:21]
 
    
 
-#     for row in b:
+    for row in b:
         
-#         print( ' - Автор: ' + row[3] + ' - Название книги: ' + row[1] + ' - Кол-во выдач: ' + row[8])
-import csv
-
-with open('books.csv', 'r', encoding="cp1251", errors="ignore") as f:
-    fr = csv.reader(f, delimiter=';')
-    res = (sorted(fr, reverse=True,  key=lambda fr: fr[]))
-
-    num = 0
-
-    for i in res:
-        num += 1
-        print(str(num) + ' - Автор: ' + i[3] + ' - Название книги: ' + i[1] + ' - Кол-во выдач: ' + i[8])
+        print( ' - Автор: ' + row[3] + ' - Название книги: ' + row[1] + ' - Кол-во выдач: ' + row[8])
