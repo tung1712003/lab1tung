eb11c085f2b55965f5275456fb7bba07cc07f14f
tung1712003 <lethanhtung1712003@gmail.com>
2023-10-18 10:35:22 +0300

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,16 +1,16 @@
 import csv
 import random
+
 with open('books.csv', 'r') as file:
-    
-    a = csv.DictReader(file, delimiter=';')
-    
-    b = random.sample(list(a), 20)
+    reader = csv.DictReader(file, delimiter=';')
+    books = random.sample(list(reader), 20)
+
 with open('20.txt', 'w') as file:
-    for index, row in enumerate(b, start=1):
-        c = row['Автор']
-        d = row['Название']
-        e = row['Дата поступления']
-        f = f"{c}. {d} - {e}\n"
-        file.write(f"{index}. {f}")
+    for index, book in enumerate(books, start=1):
+        author = book['Автор']
+        title = book['Название']
+        date = book['Дата поступления']
+        line = f"{author}. {title} - {date}\n"
+        file.write(f"{index}. {line}")
 
 print("Список библиографических ссылок сохранен в файл '20.txt'.")
