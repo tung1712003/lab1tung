fef29ba378d70443ecbfe0f8ac5d203a8b070e88
tung1712003 <lethanhtung1712003@gmail.com>
2023-10-18 10:49:34 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,17 +1,21 @@
+
 import csv
+
 PRICE_LOWER_LIMIT = 150
 
 def call_book(author):
-
     with open("books.csv") as file:
         csvreader = csv.reader(file, delimiter=';')
         next(csvreader)
         
         name_field_index = 7
+        count = 0
         for row in csvreader:
-            if len(row) >=  name_field_index + 1 and author == row[3] and float(row[ name_field_index]) >= PRICE_LOWER_LIMIT :
+            if len(row) >= name_field_index + 1 and author == row[3] and float(row[name_field_index]) >= PRICE_LOWER_LIMIT:
+                count += 1
                 print(row)
 
-author = input("Enter author's name: ")
-call_book(author)
+        print(f'Кол-во записей: {count}')
 
+author = input("Введите имя автора: ")
+call_book(author)
