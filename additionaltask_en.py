67b8b57fe0eabde6dbf9d0884222d42e5f4e9329
tung1712003 <lethanhtung1712003@gmail.com>
2023-10-18 10:56:03 +0300

    Update additionaltask_en.py

--- additionaltask_en.py
+++ additionaltask_en.py
@@ -1,11 +1,14 @@
 import csv
+row[2] = Book-Author
+row[1] = Book-Title
+row[5] = Downloads
 
 with open('books-en.csv', 'r') as file:
-    a = csv.reader(file, delimiter=';')
-    b = (sorted(a, reverse=True,  key=lambda a: a[5]))[1:21]
+    reader = csv.reader(file, delimiter=';')
+    top_book = (sorted(a, reverse=True,  key=lambda a: a[5]))[1:21]
 
    
 
-    for row in b:
+    for row in top_book:
         
-        print( ' Author: ' + row[2] + ' - Name of book: ' + row[1] + ' - Downloads: ' + row[5])
+        print( ' Author: ' +  Book-Author + ' - Name of book: ' + Book-Title + ' - Downloads: ' + Downloads )
