a0b808daa895e75b901be0c91436cd23db144dcd
tung1712003 <lethanhtung1712003@gmail.com>
2023-10-18 10:18:44 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,9 +1,8 @@
 #Выдать количество записей, у которых в поле Название строка длиннее 30 символов.
 with open('books.csv', 'r') as file:
-    a = file.readlines()
-    b = 0 
-    for line in a:
-        c = line.split(';')
-        if len(c[1]) > 30:
-            b +=1
-    print("Количество записей с названием длиннее 30 символов:", b)
+    book_count = 0
+    for line in file:
+        fields = line.split(';')
+        if len(fields[1]) > 30:
+            book_count += 1
+    print("Количество записей с названием длиннее 30 символов:", book_count)
